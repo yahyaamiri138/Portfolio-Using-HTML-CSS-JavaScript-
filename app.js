dad3132e1623fbb9d1aed9ed83eb521e42f96e6f
Yahya Amiri <yahyarasa2016@gmail.com>
2023-04-30 15:16:36 +0430

    first commit

---
+++ app.js
@@ -0,0 +1,25 @@
+(function(){
+
+    const button = document.querySelectorAll('.btn')
+    const storeImages =document.querySelectorAll('.store-item')
+
+    button.forEach((button) => {
+        button.addEventListener('click', (e)=> {
+            e.preventDefault()
+            const filter = e.target.dataset.filter
+
+            storeImages.forEach((item) => {
+                if (filter === 'all'){
+                    item.style.display = 'block'
+                }else{
+                    if(item.classList.contains(filter)){
+                        item.style.display = 'block'
+                    }
+                    else{
+                        item.style.display = 'none'
+                    }
+                }
+            })
+        })
+    })
+})();
